50cf01055e55b5d89a12d8ee71c43c72a57c7db9
Benjamin Orthen <github@orthen.net>
2023-02-05 23:26:36 +0100

    Add license boilerplate to kkp.el

--- kkp.el
+++ kkp.el
@@ -9,6 +9,22 @@
 ;; URL: https://github.com/benjaminor/kkp
 ;; Package-Requires: ((emacs "27.1"))
 
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; The Kitty Keyboard Protocol (KKP) is documented here: https://sw.kovidgoyal.net/kitty/keyboard-protocol
